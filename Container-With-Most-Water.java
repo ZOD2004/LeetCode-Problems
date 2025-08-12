10b80eb76c7250c54b2cb05036d6fa44043e1bc0
Murali Prasath K <165628843+ZOD2004@users.noreply.github.com>
2025-08-12 13:38:02 +0530

    [11] [Time Beats: 96.27%] [Memory Beats: 55.34%] - LeetPush

---
+++ Container-With-Most-Water.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int max=0;
+        int i=0;
+        int j=height.length-1;
+        while(i<j){
+            if(height[i]<height[j]){
+                max=Math.max(max,height[i]*(j-i));
+                i++;
+            }
+            else{
+                max=Math.max(max,height[j]*(j-i));
+                j--;
+            }
+
+        }
+        return max;
+    }
+}
